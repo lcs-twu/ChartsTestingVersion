adad54daba073ac9fc8131d3acad12eba2c64fe9
Tom Wu <twu@lcs.on.ca>
2024-01-16 08:34:57 -0500

    Initial Commit

---
+++ ChartsTestingVersion/ChartsTestingVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsTestingVersionApp.swift
+//  ChartsTestingVersion
+//
+//  Created by Tom Wu on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct ChartsTestingVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
